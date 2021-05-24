e4b71886e52dbfe4d1de0fce1b9c8d170ced8e52
david26_28 <Fleischpita26>
2021-05-24 23:13:46 +0200

    Added test

---
+++ app/src/androidTest/java/com/example/work2play/FragmentRewardsTest.java
@@ -0,0 +1,14 @@
+package com.example.work2play;
+
+import org.junit.Test;
+import org.mockito.Mockito.mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FragmentRewardsTest {
+    @Test
+    void testMethod() {
+        final FragmentRewards fragmentRewards = mock(FragmentRewards.class);
+
+    }
+}
