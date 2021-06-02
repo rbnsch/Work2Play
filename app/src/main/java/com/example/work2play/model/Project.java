4cdd2def668926f2462120217a7df30d4c6cbd83
rbnsch <robinschmidt0@gmx.de>
2021-06-02 11:06:41 +0200

    Created project model

---
+++ app/src/main/java/com/example/work2play/model/Project.java
@@ -0,0 +1,23 @@
+package com.example.work2play.model;
+
+public class Project {
+
+    int id;
+    String title;
+
+    public Project() {
+
+    }
+
+    public Project(String title) {
+        this.title = title;
+
+    }
+
+    public Project(int id, String title) {
+        this.id = id;
+        this.title = title;
+
+    }
+
+}
