737d595dc098244e46d7f1c27217855ddb8236fe
rbnsch <robinschmidt0@gmx.de>
2021-06-08 22:32:23 +0200

    Branch for DatabaseHabit

--- app/src/main/java/com/example/work2play/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/work2play/helper/DatabaseHelper.java
@@ -59,6 +59,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+
         db.execSQL(CREATE_TABLE_REWARD);
         db.execSQL(CREATE_TABLE_TASK);
     }
